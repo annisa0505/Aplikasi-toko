3ee98892509217d9d1e621840494c8eb827a2a81
iSAiYE <iSAiYE@iSAiYE>
2021-07-26 11:16:25 +0700

    add to repo

---
+++ src/Aplikasi_Sederhana/Cek_Koneksi.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aplikasi_Sederhana;
+
+/**
+ *
+ * @author iSAiYE
+ */
+public class Cek_Koneksi {
+    
+    public static void main(String[] args) {
+        Login l = new Login ();
+        l.setVisible(true);
+    }
+}
